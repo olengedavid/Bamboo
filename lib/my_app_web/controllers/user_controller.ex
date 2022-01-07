501bd030506161cbf5896c448b15fa288d6a1cc6
David Obonyo <olengedavid@gmail.com>
2022-01-07 22:32:10 +0300

    removed comments

--- lib/my_app_web/controllers/user_controller.ex
+++ lib/my_app_web/controllers/user_controller.ex
@@ -6,11 +6,6 @@ defmodule MyAppWeb.UserController do
 
   action_fallback MyAppWeb.FallbackController
 
-  # def index(conn, _params) do
-  #   users = Accounts.list_users()
-  #   render(conn, "index.json", users: users)
-  # end
-
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
       conn
@@ -24,20 +19,4 @@ defmodule MyAppWeb.UserController do
     user = Accounts.get_user!(id)
     render(conn, "show.json", user: user)
   end
-
-  # def update(conn, %{"id" => id, "user" => user_params}) do
-  #   user = Accounts.get_user!(id)
-
-  #   with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
-  #     render(conn, "show.json", user: user)
-  #   end
-  # end
-
-  # def delete(conn, %{"id" => id}) do
-  #   user = Accounts.get_user!(id)
-
-  #   with {:ok, %User{}} <- Accounts.delete_user(user) do
-  #     send_resp(conn, :no_content, "")
-  #   end
-  # end
 end
